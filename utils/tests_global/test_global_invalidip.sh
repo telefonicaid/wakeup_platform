8c599bbb4eba8acb7f9758d15ede7cf9e654d231
Fernando Rodriguez Sela <frsela@tid.es>
2014-02-06 12:47:50 +0100

    Added little helper tests scripts using cURL

---
+++ utils/tests_global/test_global_invalidip.sh
@@ -0,0 +1,1 @@
+curl -d 'ip=1.1.2.3&port=1234&mcc=214&mnc=07' -k --key ../test_ca/TEST_CERTIFICATES/client.key --cert ../test_ca/TEST_CERTIFICATES/client.crt https://localhost/global/wakeup/v1
---
+++ utils/tests_global/test_global_invalidnet.sh
@@ -0,0 +1,1 @@
+curl -d 'ip=10.1.2.3&port=1234&mcc=21&mnc=07' -k --key ../test_ca/TEST_CERTIFICATES/client.key --cert ../test_ca/TEST_CERTIFICATES/client.crt https://localhost/global/wakeup/v1
